b1f3f64b438c34dbdbffa6a5af03ecbc4e029071
Mark Paluch <mpaluch@pivotal.io>
2019-08-07 08:48:05 +0200

    Upgrade to Vault 1.2, Spring Cloud Greenwich SR2.

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -1,10 +1,10 @@
 package hello;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.vault.core.VaultKeyValueOperationsSupport.KeyValueBackend;
 import org.springframework.vault.core.VaultSysOperations;
 import org.springframework.vault.core.VaultTemplate;
 import org.springframework.vault.core.VaultTransitOperations;
@@ -14,54 +14,55 @@ import org.springframework.vault.support.VaultResponse;
 @SpringBootApplication
 public class Application implements CommandLineRunner {
 
-    @Autowired
-    private VaultTemplate vaultTemplate;
+	@Autowired
+	private VaultTemplate vaultTemplate;
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
 
-    @Override
-    public void run(String... strings) throws Exception {
+	@Override
+	public void run(String... strings) throws Exception {
 
-        // You usually would not print a secret to stdout
-		VaultResponse response = vaultTemplate.read("secret/github");
+		// You usually would not print a secret to stdout
+		VaultResponse response = vaultTemplate
+				.opsForKeyValue("secret", KeyValueBackend.KV_2).get("github");
 		System.out.println("Value of github.oauth2.key");
-        System.out.println("-------------------------------");
-        System.out.println(response.getData().get("github.oauth2.key"));
-        System.out.println("-------------------------------");
-        System.out.println();
+		System.out.println("-------------------------------");
+		System.out.println(response.getData().get("github.oauth2.key"));
+		System.out.println("-------------------------------");
+		System.out.println();
 
-        // Let's encrypt some data using the Transit backend.
-        VaultTransitOperations transitOperations = vaultTemplate.opsForTransit();
+		// Let's encrypt some data using the Transit backend.
+		VaultTransitOperations transitOperations = vaultTemplate.opsForTransit();
 
-        // We need to setup transit first (assuming you didn't set up it yet).
-        VaultSysOperations sysOperations = vaultTemplate.opsForSys();
+		// We need to setup transit first (assuming you didn't set up it yet).
+		VaultSysOperations sysOperations = vaultTemplate.opsForSys();
 
-        if (!sysOperations.getMounts().containsKey("transit/")) {
+		if (!sysOperations.getMounts().containsKey("transit/")) {
 
-            sysOperations.mount("transit", VaultMount.create("transit"));
+			sysOperations.mount("transit", VaultMount.create("transit"));
 
-            transitOperations.createKey("foo-key");
-        }
+			transitOperations.createKey("foo-key");
+		}
 
-        // Encrypt a plain-text value
-        String ciphertext = transitOperations.encrypt("foo-key", "Secure message");
+		// Encrypt a plain-text value
+		String ciphertext = transitOperations.encrypt("foo-key", "Secure message");
 
-        System.out.println("Encrypted value");
-        System.out.println("-------------------------------");
-        System.out.println(ciphertext);
-        System.out.println("-------------------------------");
-        System.out.println();
+		System.out.println("Encrypted value");
+		System.out.println("-------------------------------");
+		System.out.println(ciphertext);
+		System.out.println("-------------------------------");
+		System.out.println();
 
-        // Decrypt
+		// Decrypt
 
-        String plaintext = transitOperations.decrypt("foo-key", ciphertext);
+		String plaintext = transitOperations.decrypt("foo-key", ciphertext);
 
-        System.out.println("Decrypted value");
-        System.out.println("-------------------------------");
-        System.out.println(plaintext);
-        System.out.println("-------------------------------");
-        System.out.println();
-    }
+		System.out.println("Decrypted value");
+		System.out.println("-------------------------------");
+		System.out.println(plaintext);
+		System.out.println("-------------------------------");
+		System.out.println();
+	}
 }
